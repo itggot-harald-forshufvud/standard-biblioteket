0199579dbd4983511a6faa39d717d58d3a098369
itggot-harald-forshufvud <harald.forshufvud@itggot.se>
2018-04-25 14:18:12 +0200

    Added the function square

---
+++ lib/square.rb
@@ -0,0 +1,13 @@
+# Public: Takes an Integer and returns the squared Integer
+# 
+# num - The Integer that is to be squared
+# 
+# Example
+#
+#   square(5)
+#   # => 25
+#
+# Returns the squared Integer
+def square(num)
+    return num * num
+end
