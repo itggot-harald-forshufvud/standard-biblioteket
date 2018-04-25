86b0cf6abbcd55350668654620a7d85f1487ceee
itggot-harald-forshufvud <harald.forshufvud@itggot.se>
2018-04-25 14:10:06 +0200

    Added previous_number

---
+++ lib/previous_number.rb
@@ -0,0 +1,13 @@
+# Public: Returns the previous Integer to chosen Integer as input
+#
+# num - The Integer which will be subtracted by one
+#
+# Examples
+#   
+#   previos_number(3)
+#   # => 2
+#
+# Return the previous number of chosen Integer
+def previous_number(num)
+return num - 1
+end
