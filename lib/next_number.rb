1ea8366fe304937f92ae02be387f6733124f7fe9
itggot-harald-forshufvud <harald.forshufvud@itggot.se>
2018-04-25 14:00:45 +0200

    Added next_number

---
+++ lib/next_number.rb
@@ -0,0 +1,13 @@
+# Public: Returns the following Integer of chosen input.
+#
+# num - The Integer to be increased by one
+#
+# Examples
+#
+#   next_number(3)
+#   # => 4
+#
+# Returns the following Integer
+def next_number(num)
+return num + 1
+end
