bb1a0859e7d6c564d62b4167c494d9bf0f6bfc0f
itggot-harald-forshufvud <harald.forshufvud@itggot.se>
2018-05-02 13:40:43 +0200

    added min_of_three

---
+++ lib/min_of_three.rb
@@ -0,0 +1,27 @@
+# Public: Compares two Integers and returns the smallest one
+#
+# num1 - The Integer which is going to be compared
+# num2 - The Integer which is also going to be compare
+# num3 - The Integer whick is also going to be compared
+# min - The Integer which is the smallest of the two
+#
+# Example
+#
+#   min_of_three(1, 2, 3)
+#   # => 1
+#
+# Returns the smallest number
+def min_of_three(num1, num2, num3)
+    output = num1
+    if num1 > num2
+        output = num2
+    elsif num3 < num1
+        output = num3
+    end
+    return output
+end
+
+
+p min_of_three(1, 2, 3)
+p min_of_three(100, 2, 256)
+p min_of_three(1337, -1337, 0)
