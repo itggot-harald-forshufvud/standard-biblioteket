69b48441d789c851b0211f19461be32ba17fe062
itggot-harald-forshufvud <harald.forshufvud@itggot.se>
2018-05-16 11:33:51 +0200

    Started quicksort

--- dev/quick_sort.rb
+++ dev/quick_sort.rb
@@ -1,4 +1,12 @@
 def quicksort(arr)
-    i = 0
-    lower = []
+	list = arr.dup
+	i = 0
+	lower = []
+	higher = []
+	pivot = list[0]
+	list.delete_at(0)
+
+	while i < arr.length
+		if pivot < n
+
 end
